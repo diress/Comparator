f5932c7e2a15f51161f4f7a2f1b167da5b5b8897
bvasilenko <diress@gmail.com>
2016-07-15 16:55:00 +0800

    next step one

---
+++ js/comparator.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bvasilenko on 15.07.2016.
+ */
+
+$(document).ready(function(){
+
+    $('#comptable tr').each(function() {
+        $(this).find("td:first").html("123"); //не работает!
+    });
+    // for(e in items) {
+    //
+    // }
+
+});
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,12 @@
 
-$( "#target" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
-    event.preventDefault();
-});
+var items = {};
+items[1] = {name : "Слон", weight : 1000, speed : 10};
+items[2] = {name : "Гепард", weight : 90, speed : 120};
+items[3] = {name : "Черепаха", weight : 0.5, speed : 1};
+
+// $(document).ready(function(){
+//
+// })
+
+
+
