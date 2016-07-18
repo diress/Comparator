b762a45474de9703a53d9a7ac33ae4c2ff58f391
bvasilenko <diress@gmail.com>
2016-07-18 17:12:14 +0800

    second step

--- js/comparator.js
+++ js/comparator.js
@@ -4,12 +4,32 @@
 
 $(document).ready(function(){
 
-
-    // $('#comptable tr').each(function() {
-    //     $(this).find("td:first").html("123");
-    // });
+    var properties = '';
+    var header = '<tr><th>'+object_name+'</th>';
     for(e in items_properties) {
-        $('#comptable').last().append("<tr><td>"+items_properties[e].title+"</td></tr>")
+        //$('#comptable').last().append("<tr><td>"+items_properties[e].title+"</td></tr>")
+        if (items_properties[e].default) {
+            var check = 'checked';
+            header = header + '<th>' + items_properties[e].title + '</th>';
+        } else {
+            var check = '';
+        }
+        properties = properties + '<input type="checkbox" name="checkbox_' + e + ' value="checkbox" ' +  check + '>' + items_properties[e].title;
     }
+    header = header + '</tr>';
+    $(this).find("th:first").html(properties);
+    $('#comptable').last().append(header)
 
+    for (i = 0; i < items.length; i++) {
+        rowdata = "<tr><td>" + items[i].name + "</td>";
+
+        for(e in items_properties) {
+            if (items_properties[e].default) {
+                rowdata = rowdata + "<td>" + items[i][e] + "</td>";
+            }
+        }
+
+        rowdata = rowdata + "</tr>"
+        $('#comptable').last().append(rowdata)
+    }
 });
--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,14 @@
 
-var items = {};
-items[1] = {name : "Слон", weight : 1000, speed : 10};
-items[2] = {name : "Гепард", weight : 90, speed : 120};
-items[3] = {name : "Черепаха", weight : 0.5, speed : 1};
+var items = [
+    {name: "Слон", weight: 1000, speed: 10},
+    {name: "Гепард", weight: 90, speed: 120},
+    {name: "Черепаха", weight: 0.5, speed: 1}
+];
 
+var object_name = "Живая тварь";
 var items_properties = {};
-items_properties["name"] = {title : "Вес"};
-items_properties["speed"] = {title : "Скорость"};
-
-
-// $(document).ready(function(){
-//
-// })
+items_properties["weight"] = {title : "Вес", default : true};
+items_properties["speed"] = {title : "Скорость", default : false};
 
 
 
