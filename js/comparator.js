513523e67af082ec2dc1fdb6fd41bd7a42a3e214
bvasilenko <diress@gmail.com>
2016-07-19 17:15:54 +0800

    prototype

--- js/comparator.js
+++ js/comparator.js
@@ -1,38 +1,47 @@
 /**
  * Created by bvasilenko on 15.07.2016.
  */
-function changeProperties() {
-    items_properties["speed"].default = true;
+function changeProperties(checkbox) {
+    items_properties[checkbox.name].default = checkbox.checked;
+    updateTable();
+}
+
+function changeSort(header) {
+    selectSort(header.id);
     updateTable();
 }
 
 function updateTable(doc) {
+    sortItems();
 
     $('#comptable').remove();
 
     $('#table_place').append('<table id="comptable"><caption>Сравнение свойств объектов</caption><tr><th colspan="2"></th></tr></table>');
     var properties = '';
+    var show_count = 1;
     var header = '<tr><th>'+object_name+'</th>';
     for(e in items_properties) {
         if (items_properties[e].default) {
             var check = 'checked';
-            header = header + '<th>' + items_properties[e].title + '</th>';
+            header = header + '<th onClick=changeSort(this) id="' + e + '">' + items_properties[e].title + '</th>';
+            show_count++;
         } else {
             var check = '';
         }
-        properties = properties + '<input type="checkbox" name="checkbox_' + e +
-            ' value="checkbox" ' +  check + ' onClick="changeProperties()">' + items_properties[e].title;
+        properties = properties + '<input type="checkbox" name="' + e +
+            '" value="checkbox" ' +  check + ' onClick="changeProperties(this)">' + items_properties[e].title;
     }
     header = header + '</tr>';
-    $(doc).find("th:first").html(properties);
+    properties = '<th colspan="' + show_count + '">' + properties + '</th>'
+    $('#comptable').find("tr:first").html(properties);
     $('#comptable').last().append(header)
 
-    for (i = 0; i < items.length; i++) {
-        rowdata = "<tr><td>" + items[i].name + "</td>";
+    for (i = 0; i < result.length; i++) {
+        rowdata = "<tr><td>" + result[i].name + "</td>";
 
         for(e in items_properties) {
             if (items_properties[e].default) {
-                rowdata = rowdata + "<td>" + items[i][e] + "</td>";
+                rowdata = rowdata + "<td>" + result[i][e] + "</td>";
             }
         }
 
--- js/utils.js
+++ js/utils.js
@@ -5,10 +5,30 @@ var items = [
     {name: "Черепаха", weight: 0.5, speed: 1}
 ];
 
+var result = [];
 var object_name = "Живая тварь";
 var items_properties = {};
-items_properties["weight"] = {title : "Вес", default : true};
-items_properties["speed"] = {title : "Скорость", default : false};
+var sorter = null;
 
+items_properties["weight"] = {title : "Вес", default : true, asc : function(a,b) {return a.weight - b.weight}, desc : function(a,b) {return b.weight - a.weight}};
+items_properties["speed"] = {title : "Скорость", default : false, asc : function(a,b) {return a.speed - b.speed}, desc : function(a,b) {return b.speed - a.speed}};
 
+function selectSort(column) {
+    if (items_properties[column].sort == "none") {
+        items_properties[column].sort = "asc";
+        sorter = items_properties[column].asc;
+    }  else if(items_properties[column].sort == "asc") {
+        items_properties[column].sort = "desc";
+        sorter = items_properties[column].desc;
+    }  else {
+        items_properties[column].sort = "none";
+        sorter = null;
+    }
+}
 
+function sortItems() {
+    result = items.slice();
+    if (sorter != null) {
+        result.sort(sorter);
+    }
+}
